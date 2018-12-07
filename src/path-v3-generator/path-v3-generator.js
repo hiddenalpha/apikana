fa63725c21bfbc301135d4ee0c0cd6ef51e63a7c
Andreas.Fankhauser@post.ch <andreas.fankhauser@post.ch>
2018-12-07 14:09:55 +0100

    Setup empty tests based on requirements

--- src/path-v3-generator/path-v3-generator.js
+++ src/path-v3-generator/path-v3-generator.js
@@ -13,80 +13,80 @@ const UrlUtils = require('src/url-utils');
 
 
 function createPathV3Generator( options ) {
-	if( !options ) options = {};
-	throwIfPathV3GeneratorOptionsBad( options );
-	return {
-		"readable": createReadable,
-	};
-	function createReadable(){
-		var isRunning = false;
-		const rootNode = transformPathsToTree( options.openApi.paths );
-		const rootClassReadable = createClassReadable( "RootClassName" , rootNode );
-		return rootClassReadable;
-	}
-	function throwIfPathV3GeneratorOptionsBad( options ){
-		if( !options.openApi ) throw Error("Arg 'options.openApi' missing.");
+    if( !options ) options = {};
+    throwIfPathV3GeneratorOptionsBad( options );
+    return {
+        "readable": createReadable,
+    };
+    function createReadable(){
+        var isRunning = false;
+        const rootNode = transformPathsToTree( options.openApi.paths );
+        const rootClassReadable = createClassReadable( "RootClassName" , rootNode );
+        return rootClassReadable;
+    }
+    function throwIfPathV3GeneratorOptionsBad( options ){
+        if( !options.openApi ) throw Error("Arg 'options.openApi' missing.");
 
-		if( !options.javaPackage ) throw Error("Arg 'options.javaPackage' missing.");
-		if( typeof(options.javaPackage) != "string" ) throw Error( "Arg 'options.javaPackage' string expected but got '"+typeof(options.javaPackage)+"'" );
-		if( !/^(?![0-9])(?!.*\.[0-9])[A-Za-z0-9.]+$/.test(options.javaPackage) ) throw Error( "Illegal chars in javaPackage" );
-	}
+        if( !options.javaPackage ) throw Error("Arg 'options.javaPackage' missing.");
+        if( typeof(options.javaPackage) != "string" ) throw Error( "Arg 'options.javaPackage' string expected but got '"+typeof(options.javaPackage)+"'" );
+        if( !/^(?![0-9])(?!.*\.[0-9])[A-Za-z0-9.]+$/.test(options.javaPackage) ) throw Error( "Illegal chars in javaPackage" );
+    }
 }
 
 
 function createClassReadable( name , node ){
 
-	const segmentStack = Array.isArray(name) ? name : [name];
+    const segmentStack = Array.isArray(name) ? name : [name];
 
-	// Setup a readable for every subClass
-	const subClasses = [];
-	Object.keys( node ).forEach(function( subName ){
-		// Go recursive here.
-		subClasses.push(createClassReadable( segmentStack.concat(subName) , node[subName] ));
-	});
+    // Setup a readable for every subClass
+    const subClasses = [];
+    Object.keys( node ).forEach(function( subName ){
+        // Go recursive here.
+        subClasses.push(createClassReadable( segmentStack.concat(subName) , node[subName] ));
+    });
 
-	// Create our own readable which also will consume above prepared streams
-	// to embed them as nested classes.
-	var isRunning = false;
-	const readable = new stream.Readable({ read:function read( n ){
-		if( isRunning ){ return; }else{ isRunning=true; }
-		readable.push( "public static class "+ name +" {\n" ); // Start of class
-		readable.push( "\tprivate "+ name +"(){}\n" ); // Private ctor
-		readable.push( "\n" );
-		// This classes constants
-		readable.push( '\tpublic static final String PATH = BASE_PATH + "/'+ segmentStack.join('/') +'";\n' );
-		//
-		readable.push( "\n" );
-		(function loopNextSubClass( iSubClass ){
-			if( iSubClass < subClasses.length ){
-				const subClass = subClasses[iSubClass];
-				handleSubClass( subClass , loopNextSubClass.bind(0,iSubClass+1) );
-			}else{ // EndOfLoop
-				onSubClassesPrinted();
-			}
-		}( 0 ));
-		function onSubClassesPrinted(){
-			readable.push( "}\n" );
-			readable.push( null );
-		}
-	}});
+    // Create our own readable which also will consume above prepared streams
+    // to embed them as nested classes.
+    var isRunning = false;
+    const readable = new stream.Readable({ read:function read( n ){
+        if( isRunning ){ return; }else{ isRunning=true; }
+        readable.push( "public static class "+ name +" {\n" ); // Start of class
+        readable.push( "\tprivate "+ name +"(){}\n" ); // Private ctor
+        readable.push( "\n" );
+        // This classes constants
+        readable.push( '\tpublic static final String PATH = BASE_PATH + "/'+ segmentStack.join('/') +'";\n' );
+        //
+        readable.push( "\n" );
+        (function loopNextSubClass( iSubClass ){
+            if( iSubClass < subClasses.length ){
+                const subClass = subClasses[iSubClass];
+                handleSubClass( subClass , loopNextSubClass.bind(0,iSubClass+1) );
+            }else{ // EndOfLoop
+                onSubClassesPrinted();
+            }
+        }( 0 ));
+        function onSubClassesPrinted(){
+            readable.push( "}\n" );
+            readable.push( null );
+        }
+    }});
 
-	function handleSubClass( subClass , doneCback ){
-		readable.push( "\t" );
-		subClass.on( "data" , function( chunk ){
-			// Append intentation
-			chunk = chunk.toString().replace( /\n/g , '\n\t' );
-			// Then write back as our own data.
-			readable.push( chunk );
-		});
-		subClass.on( "end" , function(){
-			// Continue with next sub class when current is done.
-			readable.push( "\n" );
-			doneCback();
-		});
-	}
+    function handleSubClass( subClass , doneCback ){
+        readable.push( "\t" );
+        subClass.on( "data" , function( chunk ){
+            // Append intentation
+            chunk = chunk.toString().replace( /\n/g , '\n\t' );
+            // Then write back as our own data.
+            readable.push( chunk );
+        });
+        subClass.on( "end" , function(){
+            // Continue with next sub class when current is done.
+            readable.push( "\n" );
+            doneCback();
+        });
+    }
 
-	return readable;
+    return readable;
 }
 
 
@@ -98,15 +98,15 @@ function createClassReadable( name , node ){
  *		A tree representing the passed in paths.
  */
 function transformPathsToTree( paths ){
-	const segments2d = splitAllPathsToArrays( paths );
-	// Drop leading segment and ensure it was empty.
-	for( var i=0 ; i<segments2d.length ; ++i ){
-		if( segments2d[i].splice(0,1)[0] != "" ){
-			throw Error( "IllegalArgument: Leading slash missing on path '"+segments2d[i].join('/')+"'" );
-		}
-	}
-	const rootNode = arrange2dSegmentsAsTree( segments2d );
-	return rootNode;
+    const segments2d = splitAllPathsToArrays( paths );
+    // Drop leading segment and ensure it was empty.
+    for( var i=0 ; i<segments2d.length ; ++i ){
+        if( segments2d[i].splice(0,1)[0] != "" ){
+            throw Error( "IllegalArgument: Leading slash missing on path '"+segments2d[i].join('/')+"'" );
+        }
+    }
+    const rootNode = arrange2dSegmentsAsTree( segments2d );
+    return rootNode;
 }
 
 
@@ -117,12 +117,12 @@ function transformPathsToTree( paths ){
  * @return {Array<Array<string>>}
  */
 function splitAllPathsToArrays( paths ){
-	const keys = Object.keys( paths );
-	result = [];
-	for( var i=0 ; i<keys.length ; ++i ){
-		result[i] = keys[i].split( "/" );
-	}
-	return result;
+    const keys = Object.keys( paths );
+    result = [];
+    for( var i=0 ; i<keys.length ; ++i ){
+        result[i] = keys[i].split( "/" );
+    }
+    return result;
 }
 
 
@@ -133,40 +133,40 @@ function splitAllPathsToArrays( paths ){
  *		Nested objects where the property names represent a path segment.
  */
 function arrange2dSegmentsAsTree( paths ){
-	return toNode( null , {} , 0 );
-	function toNode( name , node , level ){
-		for( var i=0 ; i<paths.length ; ++i ){
-			const parentName = paths[i][level-1];
-			const segment = paths[i][level];
-			if( !segment ) continue;
-			if( parentName != name ) continue;
-			if( !node[segment] ) node[segment] = {};
-			toNode( segment , node[segment] , level+1 );
-		}
-		return node;
-	}
+    return toNode( null , {} , 0 );
+    function toNode( name , node , level ){
+        for( var i=0 ; i<paths.length ; ++i ){
+            const parentName = paths[i][level-1];
+            const segment = paths[i][level];
+            if( !segment ) continue;
+            if( parentName != name ) continue;
+            if( !node[segment] ) node[segment] = {};
+            toNode( segment , node[segment] , level+1 );
+        }
+        return node;
+    }
 }
 
 function pathToConstantName( path ) {
-	var segments = path.replace(/(^\/|\/$)/,"").split( '/' );
-	for( var i=0 ; i<segments.length ; ++i ){
-		const segment = segments[i];
-		if( pathSegmentIsVariable(segment) ){
-			// Drop surrounding curly braces.
-			segments[i] = segment.substr( 1 , segment.length-2 );
-		}else{
-			segments[i] = segment.toUpperCase();
-		}
-		segments[i] = segments[i]
-			// Replace invalid chars
-			.replace( /[^[A-Za-z0-9$_]/g , '_' )
-			// Also replace leading numbers
-			.replace( /^[0-9]/g , '_' )
-		;
-	}
-	return segments.join( '_' );
+    var segments = path.replace(/(^\/|\/$)/,"").split( '/' );
+    for( var i=0 ; i<segments.length ; ++i ){
+        const segment = segments[i];
+        if( pathSegmentIsVariable(segment) ){
+            // Drop surrounding curly braces.
+            segments[i] = segment.substr( 1 , segment.length-2 );
+        }else{
+            segments[i] = segment.toUpperCase();
+        }
+        segments[i] = segments[i]
+            // Replace invalid chars
+            .replace( /[^[A-Za-z0-9$_]/g , '_' )
+            // Also replace leading numbers
+            .replace( /^[0-9]/g , '_' )
+        ;
+    }
+    return segments.join( '_' );
 }
 
 function pathSegmentIsVariable( segment ) {
-	return /^{.*}$/.test( segment );
+    return /^{.*}$/.test( segment );
 }
--- test/specs/path-v3-generator.spec.js
+++ test/specs/path-v3-generator.spec.js
@@ -7,68 +7,372 @@ const PathV3Utils = require("src/path-v3-generator/pathV3Utils");
 describe( "path-v3-generator" , ()=>{
 
 
-	it( "Failfast when get invoked with an illegal javaPackage" , ( done )=>{
-		const illegalPackages = [
-			"space.not allowed", "hyphen-not.allowed", "+", "-", true,
-			""/*empty string*/, "42", "com.9number",
-		];
-		for( var i=0 ; i<illegalPackages.length ; ++i ){
-			const javaPackage = illegalPackages[ i ];
-			try{
-				PathV3Generator.createPathV3Generator({ openApi:{} , javaPackage:javaPackage });
-				expect( "Throw an Error when using javaPackage=\""+javaPackage+"\"" ).toBe( "behavior" );
-			}catch( err ){
-				expect( err.message ).toMatch( /javaPackage/i );
-			}
-		}
-		done();
-	});
+    it( "Failfast when get invoked with an illegal javaPackage" , ( done )=>{
+        const illegalPackages = [
+            "space.not allowed", "hyphen-not.allowed", "+", "-", true,
+            ""/*empty string*/, "42", "com.9number",
+        ];
+        for( var i=0 ; i<illegalPackages.length ; ++i ){
+            const javaPackage = illegalPackages[ i ];
+            try{
+                PathV3Generator.createPathV3Generator({ openApi:{} , javaPackage:javaPackage });
+                expect( "Throw an Error when using javaPackage=\""+javaPackage+"\"" ).toBe( "behavior" );
+            }catch( err ){
+                expect( err.message ).toMatch( /javaPackage/i );
+            }
+        }
+        done();
+    });
 
 
-	xit( "Only uses valid chars for Java identifiers in generated constants" , ( done )=>{
-	});
+    it( "Every path constant wears a leading slash and no trailing one" , ( done )=>{
+        const api = {
+            "paths": {
+                "/store-inventory": null,
+                "/2nd/try": null,
+                "/what.about.dots": null,
+                "/are/you/sure?": null,
+                "/are/you/a-genious": null,
+                "/a space": null,
+            }
+        };
+
+        // Setup & configure a generator instance.
+        const victim = PathV3Generator.createPathV3Generator({
+            openApi: api,
+            javaPackage: "com.example",
+        });
+
+        victim.readable()
+            .pipe( PathV3Utils.createStringWritable() )
+            .then( assertResult )
+        ;
+
+        function assertResult( result ){
+            const lines = result.split( "\n" );
+            var pathCount = 0;
+            for( var iLine=0 ; iLine<lines.length ; ++iLine ){
+                const line = lines[ iLine ];
+                // Extract the generated path.
+                const groups = /.*public static final String PATH = BASE_PATH \+ "(.+)";$/.exec( line );
+                if( groups == null ){ continue; }
+                const value = groups[1];
+                expect( value ).toMatch( /^\// ); // MUST HAVE leading slash.
+                expect( value ).toMatch( /[^\/]$/ ); // MUST NOT HAVE trailing slash.
+                pathCount += 1;
+            }
+            expect( pathCount ).toBe( 10 );
+            done();
+        }
+    });
 
 
-	it( "Every path constant wears a leading slash and no trailing one" , ( done )=>{
-		const api = {
-			"paths": {
-				"/store-inventory": null,
-				"/2nd/try": null,
-				"/what.about.dots": null,
-				"/are/you/sure?": null,
-				"/are/you/a-genious": null,
-				"/a space": null,
-			}
-		};
+    xit( "Generates path classes named by scheme DomainNameApi" , function( done ){
+        const victim = PathV3Generator.createPathV3Generator({
+            openApi: {
+                paths: {}
+            },
+            javaPackage: "com.example"
+        });
 
-		// Setup & configure a generator instance.
-		const victim = PathV3Generator.createPathV3Generator({
-			openApi: api,
-			javaPackage: "com.example",
-		});
+        victim.readable()
+            .pipe( PathV3Utils.createStringWritable() )
+            .then( assertResult )
+        ;
 
-		victim.readable()
-			.pipe( PathV3Utils.createStringWritable() )
-			.then( assertResult )
-		;
+        function assertResult( result ){
+            const lines = result.split( '\n' );
+            var className = null;
+            for( var i=0 ; i<lines.length ; ++i ){
+                const line = lines[i];
+                const m = /^public static class ([A-Za-z0-9_$]+) {$/.exec( line );
+                if( m ){
+                    expect( className ).toEqual( null );
+                    className = m[1];
+                }
+            }
+            expect( className ).toEqual( "To be defined (code_20181207134301)" );
+            done();
+        }
+    });
 
-		function assertResult( result ){
-			const lines = result.split( "\n" );
-			var pathCount = 0;
-			for( var iLine=0 ; iLine<lines.length ; ++iLine ){
-				const line = lines[ iLine ];
-				// Extract the generated path.
-				const groups = /.*public static final String PATH = BASE_PATH \+ "(.+)";$/.exec( line );
-				if( groups == null ){ continue; }
-				const value = groups[1];
-				expect( value ).toMatch( /^\// ); // MUST HAVE leading slash.
-				expect( value ).toMatch( /[^\/]$/ ); // MUST NOT HAVE trailing slash.
-				pathCount += 1;
-			}
-			expect( pathCount ).toBe( 10 );
-			done();
-		}
-	});
+
+    xit( "Generates path classes which reside in package 'com.example.lib.my.api.v1.path'" , function( done ){
+        const victim = PathV3Generator.createPathV3Generator({
+            openApi: {
+                paths: {}
+            },
+            javaPackage: "com.example.lib.my.api.v1"
+        });
+
+        victim.readable()
+            .pipe( PathV3Utils.createStringWritable() )
+            .then( assertResult )
+        ;
+
+        function assertResult( result ){
+            const lines = result.split( '\n' );
+            var firstNonEmptyLine = "";
+            for( var i=0 ; firstNonEmptyLine.length < 1 && i<lines.length ; ++i ){
+                firstNonEmptyLine = lines[i];
+            }
+            expect( firstNonEmptyLine ).toEqual( "package com.example.lib.my.api.v1.path;" );
+            done();
+        }
+    });
+
+
+    xit( "Uses first path segment after v1 after class" , function( done ){
+        // Original requirement:
+        // | Using dot-notation, a dev can access identifiers which are named like the first path segment after the 'v1' in his declared API.
+        // |     Eg: with path "/my/api/v1/foo" a dev could access: MyApi.foo
+        const victim = PathV3Generator.createPathV3Generator({
+            openApi: {
+                paths: {
+                    "/my/api/v1/foo": null
+                }
+            },
+            javaPackage: "com.example",
+        });
+
+        victim.readable()
+            .pipe( PathV3Utils.createStringWritable() )
+            .then( assertResult )
+        ;
+
+        function assertResult( result ){
+            const lines = result.split( '\n' );
+            var value;
+            for( var i=0 ; i<lines.length ; ++i ){
+                const m = /^    public static final String RESOURCE = "([^"]+)";$/.exec( lines[i] );
+                if( m ){
+                    value = m[1];
+                    break; // We assume, first match is the one we're searching for.
+                }
+            }
+            expect( value ).toEqual( "/my/api/v1/foo" );
+            done();
+        }
+    });
+
+
+    xit( "Provides MyApi.one.two.three when using path '/my/api/v1/one/two/three'" , function( done ){
+        // Original requirement:
+        // | On such identifiers a dev recursively can access all the path segments through similar identifiers.
+        // |     Eg: with path "/my/api/v1/one/two/three" a dev could access: MyApi.one.two.three
+        const victim = PathV3Generator.createPathV3Generator({
+            openApi: {
+                paths: {
+                    "/my/api/v1/one/two/three": null,
+                }
+            },
+            javaPackage: "com.example",
+        });
+
+        victim.readable()
+            .pipe( PathV3Utils.createStringWritable() )
+            .then( assertResult )
+        ;
+
+        function assertResult( result ){
+            expect( "Has-no-asserts" ).toEqual( "Has-good-asserts" );
+            done();
+        }
+    });
+
+
+    xit( "Provides RESOURCE identifier with leading, but without trailing slash" , function( done ){
+        // Original requirement:
+        // |   On every such identifier there's a reserved identifier "RESOURCE" available. This identifier is a String containing the full path, without a trailing slash.
+        const victim = PathV3Generator.createPathV3Generator({
+            openApi: {
+                paths: {}
+            },
+            javaPackage: "com.example",
+        });
+
+        victim.readable()
+            .pipe( PathV3Utils.createStringWritable() )
+            .then( assertResult )
+        ;
+
+        function assertResult( result ){
+            expect( "No-asserts" ).toBe( "Good-asserts" );
+            done();
+        }
+    });
+
+
+    xit( "Provides COLLECTION identifier with leading and trailing slash" , function( done ){
+        // On every such identifier there's a reserved identifier "COLLECTION" available. This identifier is a String containing the full path, wearing a trailing slash.
+        const victim = PathV3Generator.createPathV3Generator({
+            openApi: {
+                paths: {}
+            },
+            javaPackage: "com.example",
+        });
+
+        victim.readable()
+            .pipe( PathV3Utils.createStringWritable() )
+            .then( assertResult )
+        ;
+
+        function assertResult( result ){
+            expect( "No-asserts" ).toBe( "Good-asserts" );
+            done();
+        }
+    });
+
+
+    xit( "Provides BASED identifier where we can continue with following-up segments" , function( done ){
+        //    Also on every segment identifier there's a "BASED" available. After this identifier, a dev can continue to list follow-up path segments same as when "BASE" wouldn't be used.
+        const victim = PathV3Generator.createPathV3Generator({
+            openApi: {
+                paths: {}
+            },
+            javaPackage: "com.example",
+        });
+
+        victim.readable()
+            .pipe( PathV3Utils.createStringWritable() )
+            .then( assertResult )
+        ;
+
+        function assertResult( result ){
+            expect( "No-asserts" ).toBe( "Good-asserts" );
+            done();
+        }
+    });
+
+
+    xit( "The BASED identifier is only available once in a chain" , function( done ){
+        //    The "BASED" identifier is only available once in this chain.
+        //        Eg: MyApiPaths.foo.BASED.bar.BASED wouldn't be possible.
+        const victim = PathV3Generator.createPathV3Generator({
+            openApi: {
+                paths: {}
+            },
+            javaPackage: "com.example",
+        });
+
+        victim.readable()
+            .pipe( PathV3Utils.createStringWritable() )
+            .then( assertResult )
+        ;
+
+        function assertResult( result ){
+            expect( "No-asserts" ).toBe( "Good-asserts" );
+            done();
+        }
+    });
+
+
+    xit( "Puts only segments after BASED identifier into the constant" , function( done ){
+        //    When using RESOURCE/COLLECTION somewhere behind BASED, the string will only contain path segments mentioned after the BASED identifier.
+        //        Eg:  Using MyApi.one.BASED.two.three.COLLECTION the path would be "/two/three/".
+        const victim = PathV3Generator.createPathV3Generator({
+            openApi: {
+                paths: {}
+            },
+            javaPackage: "com.example",
+        });
+
+        victim.readable()
+            .pipe( PathV3Utils.createStringWritable() )
+            .then( assertResult )
+        ;
+
+        function assertResult( result ){
+            expect( "No-asserts" ).toBe( "Good-asserts" );
+            done();
+        }
+    });
+
+
+    xit( "Adds a dollar sign to segments which are a variable" , function( done ){
+        //    In case a segment is a variable, it will wear a $ (dollar sign) at end of its name.
+        //        Eg: Using MyApi.one.two$.three.four$.five would result in "/my/api/v1/one/{two}/three/{four}/five".
+        //        There will either only the default or the variable be available. But not both.
+        const victim = PathV3Generator.createPathV3Generator({
+            openApi: {
+                paths: {}
+            },
+            javaPackage: "com.example",
+        });
+
+        victim.readable()
+            .pipe( PathV3Utils.createStringWritable() )
+            .then( assertResult )
+        ;
+
+        function assertResult( result ){
+            expect( "No-asserts" ).toBe( "Good-asserts" );
+            done();
+        }
+    });
+
+
+    xit( "Replaces chars not allowed in java identifiers by an underscore char" , function( done ){
+        //    If a segment contains chars not allowed in java identifiers then they will be replaced with _ (underscore).
+        const victim = PathV3Generator.createPathV3Generator({
+            openApi: {
+                paths: {}
+            },
+            javaPackage: "com.example",
+        });
+
+        victim.readable()
+            .pipe( PathV3Utils.createStringWritable() )
+            .then( assertResult )
+        ;
+
+        function assertResult( result ){
+            expect( "No-asserts" ).toBe( "Good-asserts" );
+            done();
+        }
+    });
+
+
+    xit( "Will failfast when substitution of illegal chars would produce a name conflict" , function( done ){
+        // In case of a name conflict due to this replacement, Apikana will fail-fast.
+        const victim = PathV3Generator.createPathV3Generator({
+            openApi: {
+                paths: {}
+            },
+            javaPackage: "com.example",
+        });
+
+        victim.readable()
+            .pipe( PathV3Utils.createStringWritable() )
+            .then( assertResult )
+        ;
+
+        function assertResult( result ){
+            expect( "No-asserts" ).toBe( "Good-asserts" );
+            done();
+        }
+    });
+
+
+    xit( "Prepends an additional underscore char to generated identifier when they're a reserved word in java" , function( done ){
+        //    If a segment is same as a reserved word in java then it will wear an additional _ (underscore) at the begin (end).
+        const victim = PathV3Generator.createPathV3Generator({
+            openApi: {
+                paths: {}
+            },
+            javaPackage: "com.example",
+        });
+
+        victim.readable()
+            .pipe( PathV3Utils.createStringWritable() )
+            .then( assertResult )
+        ;
+
+        function assertResult( result ){
+            expect( "No-asserts" ).toBe( "Good-asserts" );
+            done();
+        }
+    });
 
 
 });
